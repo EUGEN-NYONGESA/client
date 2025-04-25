e4073e92bfa0d972d150f9e1901a6f2201607dea
eugenyongesa@gmail.com <eugenyongesa@gmail.com>
2025-04-25 19:54:11 +0300

    PickMeApp

---
+++ src/app/role.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const role = () => {
+  return (
+    <View>
+      <Text>role</Text>
+    </View>
+  )
+}
+
+export default role
